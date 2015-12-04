73faddd78c0bef08d681a6ec4bd2a454f10b2483
NiktheNomad <hetisik_nicoly@hotmail.com>
2015-12-04 17:20:41 +0100

    added order.helper+model, changed orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,42 @@
 class OrdersController < ApplicationController
 
   def index
-    # @products = products.all
+    @orders = Order.all
+    # respond_with @order
   end
 
   def show
+    order = Order.find(order_params[:id])
+    # respond_with @order
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    # respond_with @order
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully added.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    respond_with Order.destroy(params[:id])
+  end
+
+private
+
+def order_params
+      params.require(:order).permit(:name, :description, :image_url, :color)
   end
 
 end
