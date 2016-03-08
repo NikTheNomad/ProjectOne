ac7b9061c449c3998e54cb89bda43968b6b83876
NikTheNomad <hetisik_nicoly@hotmail.com>
2016-03-08 17:35:51 +0000

    database changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # products = product.create ([{name: 'Tent'}, {name: 'Hammock'}, {name: 'Shoes'}])
+
+Product.create(:name => "Apple", :description => "all your favorite apple flavors", :image_url => "apple.jpg", :color => "green")
+Product.create(:name => "Tent", :description => "Get your outdoor tents right here", :image_url => "tent.jpeg", :color => "grey")
+Product.create(:name => "Hammock", :description => "Outdoor luxury, it can happen with these hammocks", :image_url => "hammock-1.jpg", :color => "black")
+Product.create(:name => "Bike", :description => "", :image_url => "girlbike.jpg")
+
+# Product.create(:name => "", :description => "", :image_url => "")
+
+# ..met elke db:seed wordt toegevoegd
